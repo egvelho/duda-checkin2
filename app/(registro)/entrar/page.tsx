b7aa621cebc1a3bd4c10b47577d9e5a1bedeac91
Eduarda Velho <email@eduardavelho.com>
2026-05-09 11:29:50 -0300

    nem sei o q fiz

--- app/(registro)/entrar/page.tsx
+++ app/(registro)/entrar/page.tsx
@@ -1,4 +1,11 @@
+"use client";
+
+import { useState } from "react";
+
 export default function SignInPage() {
+  const [email, setEmail] = useState("");
+  const [senha, setSenha] = useState("");
+
   return (
     <div>
       <form className="flex flex-col gap-2">
@@ -6,11 +13,19 @@ export default function SignInPage() {
           type="text"
           placeholder="Email"
           className="border rounded-md p-2"
+          value={email}
+          onChange={(event) => {
+            setEmail(event.target.value);
+          }}
         />
         <input
           type="password"
           placeholder="Senha"
           className="border rounded-md p-2"
+          value={senha}
+          onChange={(event) => {
+            setSenha(event.target.value);
+          }}
         />
         <button
           type="submit"
@@ -19,6 +34,21 @@ export default function SignInPage() {
           Entrar
         </button>
       </form>
+      <BotaoOnOff />
     </div>
+  );
+}
+
+function BotaoOnOff() {
+  const [on, setOn] = useState(true);
+  const label = on ? "Ligado" : "Desligado";
+  const estilo = on ? "bg-red-600" : "bg-blue-600";
+  return (
+    <button
+      onClick={() => setOn(!on)}
+      className={`${estilo} text-white rounded-md p-2`}
+    >
+      {label}
+    </button>
   );
 }
